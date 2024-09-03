5165dbc3c78e0590c11b17fd61d5328a6c79dcb3
rohitchandel8144 <119517759+rohitchandel8144@users.noreply.github.com>
2024-09-04 02:12:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -67,7 +67,7 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 50000, // Adjust as needed
+    // Adjust as needed
   })
   .then(() => {
     console.log("Connected to MongoDB");
