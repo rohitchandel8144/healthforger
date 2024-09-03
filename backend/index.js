6dcda1dd5cad2c23fe71fcd7e0d1279cdaa36ec1
rohitchandel8144 <119517759+rohitchandel8144@users.noreply.github.com>
2024-09-04 01:15:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -64,6 +64,18 @@ app.use("/api/payment", paymentRoutes);
 
 // Connect to MongoDB
 mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 50000, // Adjust as needed
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
+mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
